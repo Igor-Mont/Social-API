bf619de1af38fd3e81124f8d9fe51c3970d7a52c
Igor Mont <igornathandev@gmail.com>
2022-05-13 22:48:18 -0300

    chore: add swagger

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config'
+import swaggerUI from 'swagger-ui-express'
+import swaggerFile from '../../../swagger.json'
 import express, { json } from 'express'
 
 const app = express()
 
 app.use(json())
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 export { app }
